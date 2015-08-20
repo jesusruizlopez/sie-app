5354fe5b137976e60a191ce7d9891cb004e9ce21
Jesús Ruiz <jesusamadoruizlopez@gmail.com>
2015-08-20 10:54:39 -0600

    Modificaciones Courses

--- src/modules/courses/courses.js
+++ src/modules/courses/courses.js
@@ -8,5 +8,26 @@ angular.module('courses', [])
 }])
 
 .controller('CoursesCtrl', ['$scope', function($scope) {
+	$scope.courses = [
+		{
+			id: "12DE34FR",
+			name: "Programación Web",
+			description: "Curso avanzado de programación web",
+			responsible: "Martín Nevarez"
+		},
+		{
+			id: "12EE14ZR",
+			name: "Matemáticas",
+			description: "Curso avanzado de Matemáticas",
+			responsible: "Felix de la Rocha"
+		}
+	];
 
+	$scope.edit = function(id) {
+		console.log(id);
+	};
+
+	$scope.remove = function(course) {
+		console.log(course);
+	};
 }]);
